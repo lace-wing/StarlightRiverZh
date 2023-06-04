047637aff5a4913ead594cbe95cfadefec96b657
lyc-Lacewing <lycrlsu01@gmail.com>
2023-06-04 09:45:27 +0800

    ?

--- Notifications.cs
+++ Notifications.cs
@@ -6,13 +6,12 @@ using System.Threading.Tasks;
 using Terraria;
 using Terraria.ModLoader
 
-namespace StarlightRiverZh
+namespace StarlightRiverZh;
+
+internal class Notifications : ModPlayer
 {
-    internal class Notifications : ModPlayer
+    public override void OnEnterWorld()
     {
-        public override void OnEnterWorld()
-        {
-            Main.NewText("欢迎使用由星光之舟汉化组制作的星光之河汉化补丁。制作仓促，目前翻译尚不完善。我们会逐渐改进，感谢您的耐心和理解。");
-        }
+        Main.NewText("欢迎使用由星光之舟汉化组制作的星光之河汉化补丁。制作仓促，目前翻译尚不完善。我们会逐渐改进，感谢您的耐心和理解。");
     }
 }
