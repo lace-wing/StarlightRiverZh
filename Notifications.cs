732c67d46b0d68c34e30858067978a9c0e85003e
lyc-Lacewing <lycrlsu01@gmail.com>
2023-06-04 09:50:51 +0800

    note

--- Notifications.cs
+++ Notifications.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,7 @@ namespace StarlightRiverZh
     {
         public override void OnEnterWorld()
         {
-            Main.NewText("欢迎使用由星光之舟汉化组制作的星光之河汉化补丁。制作仓促，目前翻译尚不完善。我们会逐渐改进，感谢您的耐心和理解。");
+            Main.NewText("欢迎使用由星光之舟汉化组制作的星光之河汉化补丁。 制作仓促，目前翻译尚不完善。 我们会逐渐改进，感谢您的耐心和理解。", Color.YellowGreen);
         }
     }
 }
