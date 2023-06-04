4fe4a7d162365443f749c7ebb3f7ff155b52d75a
lyc-Lacewing <lycrlsu01@gmail.com>
2023-06-04 09:45:45 +0800

    wssb

--- Notifications.cs
+++ Notifications.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria;
-using Terraria.ModLoader
+using Terraria.ModLoader;
 
-namespace StarlightRiverZh;
+namespace StarlightRiverZh
 
 internal class Notifications : ModPlayer
 {
