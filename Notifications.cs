eabcc7c48cb9b71c5aabbe4013599c4834ef2565
lyc-Lacewing <lycrlsu01@gmail.com>
2023-06-05 18:20:16 +0800

    only show changeog if mod version changed

--- Notifications.cs
+++ Notifications.cs
@@ -9,15 +9,36 @@ using Terraria;
 using Terraria.Localization;
 using Terraria.ModLoader;
 using StarlightRiverZh.Utils;
+using Terraria.ModLoader.IO;
 
 namespace StarlightRiverZh
 {
     internal class Notifications : ModPlayer
     {
+        public static string LastVersion;
         public override void OnEnterWorld()
         {
             Main.NewText(Text.GetTaggedText(Text.NotifPath + "Welcome", Color.YellowGreen));
-            string log, notifLog;
+            if (LastVersion != Mod.Version.ToString())
+            {
+                PopChangelog();
+            }
+        }
+        public override void SaveData(TagCompound tag)
+        {
+            tag.Add("LastVersion", Mod.Version.ToString());
+        }
+        public override void LoadData(TagCompound tag)
+        {
+            if (tag.ContainsKey("LastVersion"))
+            {
+                LastVersion = tag.GetString("LastVersion");
+            }
+        }
+
+        public void PopChangelog()
+        {
+            string log;
             var stream = Mod.GetFileStream("changelog.txt");
             try
             {
@@ -28,10 +49,10 @@ namespace StarlightRiverZh
             {
                 throw;
             }
-            notifLog = log == string.Empty
+            log = log == string.Empty
                 ? Text.GetTaggedText(Text.NotifPath + "NoChangelog", Color.YellowGreen, new object[] { Mod.Version })
-                : Text.GetTaggedText(Text.NotifPath + "Changelog", Color.YellowGreen, new object[] {Mod.Version, log});
-            Main.NewTextMultiline(notifLog, true);
+                : Text.GetTaggedText(Text.NotifPath + "Changelog", Color.YellowGreen, new object[] { Mod.Version, log });
+            Main.NewTextMultiline(log, true);
         }
     }
 }
