5f1a1f02eada8c66ab328158042c06351f4148ed
lyc-Lacewing <lycrlsu01@gmail.com>
2023-06-05 18:09:53 +0800

    text utils and new welcome texts

--- Notifications.cs
+++ Notifications.cs
@@ -6,7 +6,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria;
+using Terraria.Localization;
 using Terraria.ModLoader;
+using StarlightRiverZh.Utils;
 
 namespace StarlightRiverZh
 {
@@ -14,8 +16,8 @@ namespace StarlightRiverZh
     {
         public override void OnEnterWorld()
         {
-            Main.NewText("欢迎使用由星光之舟汉化组制作的星光之河汉化补丁。 制作仓促，目前翻译尚不完善。 我们会逐渐改进，感谢您的耐心和理解。", Color.YellowGreen);
-            string log;
+            Main.NewText(Text.GetTaggedText(Text.NotifPath + "Welcome", Color.YellowGreen));
+            string log, notifLog;
             var stream = Mod.GetFileStream("changelog.txt");
             try
             {
@@ -26,8 +28,10 @@ namespace StarlightRiverZh
             {
                 throw;
             }
-            log = log == string.Empty ? $"{Mod.Version} 未检测到更新日志" : log.Insert(0, $"v{Mod.Version} 更新日志\n");
-            Main.NewTextMultiline(log, true, Color.YellowGreen);
+            notifLog = log == string.Empty
+                ? Text.GetTaggedText(Text.NotifPath + "NoChangelog", Color.YellowGreen, new object[] { Mod.Version })
+                : Text.GetTaggedText(Text.NotifPath + "Changelog", Color.YellowGreen, new object[] {Mod.Version, log});
+            Main.NewTextMultiline(notifLog, true);
         }
     }
 }
---
+++ Utils/Text.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.Localization;
+
+namespace StarlightRiverZh.Utils
+{
+    public static class Text
+    {
+        public const string DedPath = "Mods.StarlightRiverZh.";
+        public const string LocPath = "Mods.StarlightRiver.";
+
+        public const string NotifPath = DedPath + "Notifications.";
+
+        public static string TagColor(this string text, Color color)
+        {
+            text = text.ReplaceLineEndings("\n");
+            text = text.Insert(text.Length, "]");
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                if (text[i] != '\n')
+                {
+                    continue;
+                }
+                text = text.Insert(i + 1, $"[c/{color.Hex3()}:");
+                text = text.Insert(i, "]");
+            }
+            text = text.Insert(0, $"[c/{color.Hex3()}:");
+            return text;
+        }
+        public static string GetTaggedText(string path, Color color = default, object[] args = default)
+        {
+            if (args != null)
+            {
+                return Language.GetTextValue(path, args).TagColor(color);
+            }
+            return Language.GetTextValue(path).TagColor(color);
+        }
+    }
+}
