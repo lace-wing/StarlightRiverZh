1f191b43205816f95938b5e0b421bac517cfe546
Sky-Meteor <2746501855@qq.com>
2023-06-10 13:52:52 +0800

    fix permafrost shrine tip

--- QuickTranslate/Entries/GamePlay/PermafrostShrine.cs
+++ QuickTranslate/Entries/GamePlay/PermafrostShrine.cs
@@ -5,8 +5,8 @@ namespace StarlightRiverZh.QuickTranslate.Entries.GamePlay;
 public class PermafrostShrine : MethodEntry
 {
     public PermafrostShrine() : base("DoorBombShooter", "PostDraw") { }
-    
-    public override void Load()
+
+    public override void ILLoad()
     {
         QTTM("Place blocks on", "把方块放置在");
         QTTM("BLUE", "蓝色");
