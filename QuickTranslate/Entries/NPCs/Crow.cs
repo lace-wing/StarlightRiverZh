3847f10c85e769fee709a57c138970873b1b98e9
lyc-Lacewing <lycrlsu01@gmail.com>
2023-07-01 23:07:40 +0800

    update slr.dll

---
+++ QuickTranslate/Entries/NPCs/Crow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarlightRiverZh.QuickTranslate.Entries.NPCs
+{
+    internal class Crow : TypeEntry
+    {
+        public Crow(Type targetType) : base(StarlightRiver.Content.NPCs.)
+        {
+        }
+    }
+}
